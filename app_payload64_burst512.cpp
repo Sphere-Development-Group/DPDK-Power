c37cf38f886252f74cf54211dbeaaddc9eb43c1a
Tony Lysenko <udhfbb@yandex.ru>
2026-05-11 23:00:29 +0700

    Коррекция в конфигурации портов

--- app_payload64_burst512.cpp
+++ app_payload64_burst512.cpp
@@ -108,8 +108,8 @@ int main(int argc, char* argv[]) {
       port_conf[port_id].txmode.offloads |= RTE_ETH_TX_OFFLOAD_TCP_CKSUM;
     }
 
-    if (rte_eth_dev_configure(port_id, 0,
-                              0,
+    if (rte_eth_dev_configure(port_id, port_info[port_id].max_rx_queues,
+                              port_info[port_id].max_tx_queues,
                               &port_conf[port_id]) < 0)
       rte_exit(rte_errno, "Ошибка конфигурирования интерфейса: %s\n",
                rte_strerror(rte_errno));
@@ -118,7 +118,7 @@ int main(int argc, char* argv[]) {
     for (int rx_queue = 0; rx_queue < 1;
          rx_queue++) {
       int rx_queue_cr = rte_eth_rx_queue_setup(
-          port_id, rx_queue, 1,
+          port_id, rx_queue, port_info[port_id].rx_desc_lim.nb_max,
           rte_eth_dev_socket_id(port_id), NULL, rx_pool[port_id]);
       if (rx_queue_cr != 0)
         rte_exit(rte_errno, "Ошибка настройки RX очереди: %s\n",
@@ -128,7 +128,7 @@ int main(int argc, char* argv[]) {
     for (int tx_queue = 0; tx_queue < 1;
          tx_queue++) {
       int tx_queue_cr = rte_eth_tx_queue_setup(
-          port_id, tx_queue, 1,
+          port_id, tx_queue, port_info[port_id].tx_desc_lim.nb_max,
           rte_eth_dev_socket_id(port_id), NULL);
       if (tx_queue_cr != 0)
         rte_exit(rte_errno, "Ошибка настройки TX очереди: %s\n",
